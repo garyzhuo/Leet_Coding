eca9f8fe7d1d40becb3a9717483487bc55252c77
garyzhuo <gary_zhuo@hotmail.com>
2023-05-16 14:26:42 -0500

    O(n)

--- BIgO.js
+++ BIgO.js
@@ -5,3 +5,15 @@ worst case is represented with the greek letter big O O
 
 when we are measuring Big O we are always measuring the worst case
 */
+
+/* This will be called "O of N operations"
+we are passing the function the number "n" and the function will run "n" times 
+so if we pass n as 10 it will run 10 times
+*/
+function logItems(n) {
+    for (let i = 0; i < n; i++) {
+        console.log(i)
+    }
+}
+
+logItems(10)
