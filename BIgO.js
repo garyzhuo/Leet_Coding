9f339058d356a7968d7bfb4061e31b2414c91734
garyzhuo <gary_zhuo@hotmail.com>
2023-05-16 14:18:05 -0500

    Added the meanings of BigO and we're measuring

---
+++ BIgO.js
@@ -0,0 +1,7 @@
+/*In Big O 
+the best case is represented by the greek letter omega Ω
+average case is represented with the greek letter theta Θ
+worst case is represented with the greek letter big O O  
+
+when we are measuring Big O we are always measuring the worst case
+*/
