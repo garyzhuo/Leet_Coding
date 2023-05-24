1fecc96edd48de895c10b8922e0fddfdf0240500
garyzhuo <gary_zhuo@hotmail.com>
2023-05-24 13:08:21 -0500

    DLL_Constructor_Explained

--- DLL_Constructor.js
+++ DLL_Constructor.js
@@ -4,4 +4,31 @@
 
 before this is how it would look like - 
 
+class Node {
+    constructor(value) {
+    this.value = value
+    this.next = null
+    }
+}
+
+
+with a DLL this is how it would look like 
+class Node {
+    constructor(value) {
+    this.value = value
+    this.next = null
+    this.prev = null <-- this is add 
+    }
+}
+
+class DoublyLinkedList {}
+constructor(value) {
+const newNode = new Node(value)
+this.head = newNode
+this.tail = this.head
+this.length = 1
+}
+}
+
+when this node is created it will have a next and prev 
 */
