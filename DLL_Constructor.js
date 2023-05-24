3f2939980073d58ba486b0b888323ad8a378544f
garyzhuo <gary_zhuo@hotmail.com>
2023-05-24 13:05:32 -0500

    DLL_Constructor

--- DLL_Constructor.js
+++ DLL_Constructor.js
@@ -1,1 +1,7 @@
 /* Double Linked List - Constructor */
+
+/* a doubly linked list instead of going one way --> it can go both left and right <-- and --> 
+
+before this is how it would look like - 
+
+*/
