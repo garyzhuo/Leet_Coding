831e7ffe8e70072dfde8f844849e5c81922d1c75
garyzhuo <gary_zhuo@hotmail.com>
2023-05-24 13:15:26 -0500

    DLL Push Method Explained

--- DLL_Push.js
+++ DLL_Push.js
@@ -1,2 +1,19 @@
 /* DLL PUSH */
 
+/*
+
+push(value) {
+    const newNode = newNode(value)
+    if (!this.head) {
+        this.head = newNode
+        this.tail = newNode
+    } else {
+        this.tail.next = newNode
+        newNode.prev = this.tail <-- this is only new line of code in linked list _ push method
+        this.tail = newNode
+    }
+    this.length++
+    return this
+}
+
+*/
