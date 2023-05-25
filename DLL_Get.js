435c2a723a6023fc2b6e23b5c6259185c2dfabf1
garyzhuo <gary_zhuo@hotmail.com>
2023-05-25 17:43:22 -0500

    Double Linked List - Get Method

---
+++ DLL_Get.js
@@ -0,0 +1,23 @@
+/* Double Linked List - Get Method */
+
+/* 
+this would be for a singly linked list -- 
+get (index) {
+    if (index < 0 || index >= this.length) {
+    return undefined
+    }
+    let temp = this.head
+    if (index < this.length/2) {
+    for (let i = 0; i < index; i++) {
+    temp = temp.next
+    }
+   } else {
+   temp = this.tail
+   for (let i = this.length - 1; i > index; i--) {
+   temp = temp.prev
+   }
+   }
+    return temp
+}
+
+*/
