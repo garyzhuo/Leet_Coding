cc1c7f8ef34591ed7540bd7d70db4d139c907bd4
garyzhuo <gary_zhuo@hotmail.com>
2023-05-25 18:01:21 -0500

    Double Linked List - Remove Method

---
+++ DLL_Remove.js
@@ -0,0 +1,19 @@
+/* DLL Remove Method */
+
+/*
+
+remove (index) {
+    if (index === 0) return this.shift()
+    if (index === this.length -1) return this.pop()
+    if (index < 0 || index >= this.length) return undefined
+
+    const temp = this.get(index)
+
+    temp.prev.next = temp.next
+    temp.next.prev = temp.prev
+
+    temp.next = null
+    temp.prev = null
+}
+
+*/
