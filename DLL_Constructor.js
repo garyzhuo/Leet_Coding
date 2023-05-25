de86de1d7e3bdbefe7afab4ed0a0a3c26df1ee35
garyzhuo <gary_zhuo@hotmail.com>
2023-05-25 18:16:16 -0500

    Double Linked List - Push Method

--- DLL_Constructor.js
+++ DLL_Constructor.js
@@ -35,3 +35,74 @@ once again the only difference would be instead of a single linked list going --
 this would going <-- and -->
 when this node is created it will have a next and prev 
 */
+
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+        this.prev = null
+    }
+}
+
+class DoublyLinkedList {
+    constructor(value) {
+        const newNode = new Node(value)
+        this.head = newNode
+        this.tail = this.head
+        this.length = 1
+    }
+
+
+    printList() {
+        let temp = this.head;
+        while (temp !== null) {
+            console.log(temp.value);
+            temp = temp.next;
+        }
+    }
+
+    getHead() {
+        if (this.head === null) {
+            console.log("Head: null");
+        } else {
+            console.log("Head: " + this.head.value);
+        }
+    }
+
+    getTail() {
+        if (this.tail === null) {
+            console.log("Tail: null");
+        } else {
+            console.log("Tail: " + this.tail.value);
+        }
+    }
+
+    getLength() {
+        console.log("Length: " + this.length);
+    }
+
+}
+
+
+
+let myDLL = new DoublyLinkedList(7);
+
+myDLL.getHead();
+myDLL.getTail();
+myDLL.getLength();
+
+console.log("\nDoubly Linked List:");
+myDLL.printList();
+
+
+/*
+    EXPECTED OUTPUT:
+    ----------------
+    Head: 7
+    Tail: 7
+    Length: 1
+
+    Doubly Linked List:
+    7
+
+*/
